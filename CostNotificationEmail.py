d04fe5a9d23278986d9cea7c3c95d4c45fc69d8b
Flupinochan <flupino@metalmental.net>
2023-08-04 01:44:02 +0900

    save

--- CostNotificationEmail.py
+++ CostNotificationEmail.py
@@ -18,7 +18,7 @@ import os
 import sys
 import boto3
 
-from typing import List
+from typing import Dict, List, Tuple
 from zoneinfo import ZoneInfo
 from botocore.exceptions import ClientError, BotoCoreError
 from botocore.config import Config
@@ -38,17 +38,20 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 # Retry count when using client or resource API
 RETRY_COUNT = int(os.environ.get('RETRY_COUNT', 3))
 
-# Development Master AccountID (String type is ok)
+# Master AccountID (String Type)
 ACCOUNT_ID = os.environ['ACCOUNT_ID']
 
 # Budget Name
 BUDGET_NAME = os.environ['BUDGET_NAME']
 
+# SNS ARN
+SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
+
 # ----------------------------------------------------------------
 # Global Variable Definitions
 # ----------------------------------------------------------------
 
-# Settings for retry count (fixed standard) and region (Tokyo) are specified in the arguments when using client or resource API
+# Set arguments like retry counts and region when using client or resource API
 config = Config(
     region_name='ap-northeast-1',
     retries={
@@ -61,14 +64,18 @@ config = Config(
 # Budgets API settings
 client_budgets = boto3.client('budgets', config=config)
 # Cost Explorer API settings
-client_CostExplorer = boto3.client('ce', config=config)
+client_cost_explorer = boto3.client('ce', config=config)
+# SNS API settings
+resource_sns = boto3.resource('sns', config=config)
+sns_topic = resource_sns.Topic(SNS_TOPIC_ARN)
+# Organizations API settings
+client_organizations = boto3.client('organizations')
 
 # ----------------------------------------------------------------------
 # Logger Configuration
 # ----------------------------------------------------------------------
 
-# Obtains Logger object from LoggingClass in SandboxLoggingClass.py. Used for log output.
-
+# Get Logger object. Use this for log output
 logger = LoggingClass(LOG_LEVEL)
 log = logger.get_logger()
 
@@ -80,7 +87,7 @@ log = logger.get_logger()
 # ----------------------------------------------------------------------
 def main():
 
-    # sys._getframe().f_code.co_name) becomes the function name
+    # sys._getframe().f_code.co_name) will give the function name
     log.debug("{}() Start processing".format(sys._getframe().f_code.co_name))
 
     # Get the accounts for cost aggregation
@@ -89,8 +96,15 @@ def main():
     # Get the beginning of the month required to fetch this month's cost
     start_utc_str, end_utc_str = time_processing()
 
-    # Get cost aggregation results
-    cost_data = get_cost_aggregation(account_list, start_utc_str, end_utc_str)
+    # Get account name
+    account_name_dict = get_account_name(account_list)
+
+    # Get cost ranking
+    service_cost_ranking_message = get_service_cost_ranking(account_list, start_utc_str, end_utc_str)
+    total_cost, account_cost_ranking_message = get_account_cost_ranking(account_name_dict, start_utc_str, end_utc_str)
+
+    # Send cost ranking
+    sns_publish(start_utc_str, service_cost_ranking_message, total_cost, account_cost_ranking_message)
 
     log.debug("{}() End processing".format(sys._getframe().f_code.co_name))
 
@@ -123,11 +137,11 @@ def get_account_list() -> List[str]:
 # ----------------------------------------------------------------------
 # Time Processing
 # ----------------------------------------------------------------------
-def time_processing() -> str:
+def time_processing() -> Tuple[str, str]:
 
     log.debug("{}() Start processing".format(sys._getframe().f_code.co_name))
 
-    # Beginning of the month, today's date in UTC ########### Last month for test
+    # Beginning of the month, today's date in UTC   ########### Last month for test
     now = datetime.datetime.now(UTC)
     start_utc = datetime.datetime(now.year, now.month-1, 1)
     start_utc_str = start_utc.strftime('%Y-%m-%d')
@@ -143,11 +157,11 @@ def time_processing() -> str:
 # ----------------------------------------------------------------------
 # Get Service Cost Ranking
 # ----------------------------------------------------------------------
-def get_cost_aggregation(arg_account_list, arg_start_utc_str, end_utc_str):
+def get_service_cost_ranking(arg_account_list: List[str], arg_start_utc_str: str, end_utc_str: str) -> List[str]:
 
     log.debug("{}() Start processing".format(sys._getframe().f_code.co_name))
 
-    response = client_CostExplorer.get_cost_and_usage(
+    response = client_cost_explorer.get_cost_and_usage(
         TimePeriod={
             'Start': arg_start_utc_str,
             'End': end_utc_str
@@ -170,19 +184,127 @@ def get_cost_aggregation(arg_account_list, arg_start_utc_str, end_utc_str):
         }
     )
 
+    # Array for storing data
+    service_cost_ranking_message = []
+
     # Top 5 High Cost Services
     for index, service in enumerate(sorted(
             [group for group in response['ResultsByTime'][0]['Groups'] if group['Keys'][0] != 'Tax'],
             key=lambda x: float(x['Metrics']['UnblendedCost']['Amount']),
-            reverse=True)[:5], start=1):
-        service_name = service['Keys'][0]
+            reverse=True)[:5]):
+        
+        rank = "TOP" + str(index + 1)
         service_cost_str_float = service['Metrics']['UnblendedCost']['Amount']
         service_cost = service_cost_str_float.split('.')[0]
         unit = service['Metrics']['UnblendedCost']['Unit']
+        service_name = service['Keys'][0]
 
-        log.debug("TOP{} {} : {} {}".format(index, service_name, service_cost, unit))
+
+        message = "{} {}{} : {}".format(rank, service_cost, unit, service_name)
+        service_cost_ranking_message.append(message)
+
+        log.debug(message)
 
     log.debug("{}() End processing".format(sys._getframe().f_code.co_name))
+
+    return service_cost_ranking_message
+
+
+# ----------------------------------------------------------------------
+# Get Account Cost Ranking
+# ----------------------------------------------------------------------
+def get_account_cost_ranking(arg_account_name_dict: Dict[str, str], arg_start_utc_str: str, end_utc_str: str) -> List[str]:
+
+    log.debug("{}() Start processing".format(sys._getframe().f_code.co_name))
+
+    response = client_cost_explorer.get_cost_and_usage(
+        TimePeriod={
+            'Start': arg_start_utc_str,
+            'End': end_utc_str
+        },
+        Granularity='MONTHLY',
+        Metrics=[
+            'UnblendedCost',
+        ],
+        GroupBy=[
+            {
+                'Type': 'DIMENSION',
+                'Key': 'LINKED_ACCOUNT'
+            },
+        ]
+    )
+
+    total_cost_int = 0
+    account_cost_ranking_message = []
+
+    for index, group in enumerate(sorted(
+        [group for group in response['ResultsByTime'][0]['Groups'] if group['Keys'][0] != 'Tax'],
+        key=lambda x: float(x['Metrics']['UnblendedCost']['Amount']),
+        reverse=True)[:2]):
+
+        rank = "TOP" + str(index + 1)
+        account_id = group['Keys'][0]
+        account_cost_str_float = group['Metrics']['UnblendedCost']['Amount']
+        account_cost = account_cost_str_float.split('.')[0]
+        unit = group['Metrics']['UnblendedCost']['Unit'] 
+
+        message = "{} {}{} : {}({})".format(rank, account_cost, unit, arg_account_name_dict[account_id], account_id)
+        account_cost_ranking_message.append(message)
+    
+    for group in response['ResultsByTime'][0]['Groups']:
+        total_cost_int += int(float(group['Metrics']['UnblendedCost']['Amount']))
+    
+    total_cost = str(total_cost_int)
+
+    return total_cost, account_cost_ranking_message
+
+
+
+
+# ----------------------------------------------------------------------
+# Get Account Name
+# ----------------------------------------------------------------------
+def get_account_name(arg_account_list) -> Dict[str, str]:
+
+    account_name_dict = {}
+
+    paginator = client_organizations.get_paginator('list_accounts')
+    for page in paginator.paginate():
+        for account in page['Accounts']:
+            for accountId in arg_account_list:
+                if account['Id'] == accountId:
+                    account_name_dict[account['Id']] = account['Name']
+
+    return account_name_dict
+
+
+# ----------------------------------------------------------------------
+# SNS(send email)
+# ----------------------------------------------------------------------
+def sns_publish(arg_start_utc_str, arg_service_cost_ranking_message, arg_total_cost, arg_account_cost_ranking_message) -> None:
+
+    log.debug("{}() Start processing".format(sys._getframe().f_code.co_name))
+
+
+
+    # 件名と本文を作成
+    # sns_message = '\n'.join(arg_service_cost_ranking_message)
+    # sns_subject = "【Cost Notification】{}".format(arg_start_utc_str)
+    sns_message = '\n'.join(arg_account_cost_ranking_message)
+    sns_subject = "Total Cost : {}".format(arg_total_cost)
+
+    # メール送信
+    log.debug("SNS メール送信 本文 : {}.....".format(arg_service_cost_ranking_message[0]))
+    log.info("SNS メール送信 件名 : {}".format(sns_subject))
+
+    sns_topic.publish(
+        Message = sns_message,
+        Subject = sns_subject
+    )
+
+    log.debug("{}() End processing".format(sys._getframe().f_code.co_name))
+
+
 
 # ----------------------------------------------------------------------
 # Entry Point (Program Start Point)
@@ -197,21 +319,21 @@ def lambda_handler_entrypoint(event, context):
     except ClientError as e:
         # Error handling for AWS API
         error_message = e.response['Error']['Message']
-        log.error("An error occurred in the AWS API. There may be an error in the CloudWatch LogGroup name, SNS Topic name, or you may not have access rights.")
+        log.error("An error occurred with the AWS API.")
         log.error(error_message, exc_info=True)
 
     except BotoCoreError as e:
-        # Error handling for the boto3 SDK itself
+        # Error handling for boto3 SDK
         error_message = str(e)
-        log.error("An error occurred in the boto3 library.")
+        log.error("An error occurred with the boto3 library.")
         log.error(error_message, exc_info=True)
 
     except Exception as e:
-        # Unexpected error handling
+        # Error handling for other unexpected errors
         error_message = str(e)
         log.error("An unexpected error occurred. There may be a syntax error in the code.")
         log.error(error_message, exc_info=True)
 
     finally:
-        log.debug("{}() End processing".format(sys._getframe().f_code.co_name))
-        log.info('The processing was completed successfully')
+        log.debug("{}() Process end".format(sys._getframe().f_code.co_name))
+        log.info('Processing completed successfully')
